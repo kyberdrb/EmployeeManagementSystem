c1cf0fa7495d5409d2c969fcdb31b7db6a58e179
kyberdrb <kyberdrb@gmail.com>
2020-07-14 19:27:07 +0200

    Add code coverage for unit tests

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ SCRIPT_FULL_PATH=$(realpath "$0")
 PROJECT_FULL_PATH=$(dirname "$SCRIPT_FULL_PATH")
 
 # clean()
-# remove core dumps from Valgrind
-rm $PROJECT_FULL_PATH/vgcore.*
+rm $PROJECT_FULL_PATH/vgcore.* # remove core dumps from Valgrind
 
 BUILD_DIR_FULL_PATH="${PROJECT_FULL_PATH}/build"
 
@@ -29,7 +28,7 @@ cmake -DPROJECT_NAME=${EXECUTABLE_NAME} -S "$PROJECT_FULL_PATH" -B "$BUILD_DIR_F
 # CMake with clang-tidy - static analysis for inspiration and safety
 # cmake -DPROJECT_NAME=${EXECUTABLE_NAME} "-DCMAKE_CXX_CLANG_TIDY=/usr/bin/clang-tidy;-checks=*;-header-filter=.*" -S "$PROJECT_FULL_PATH" -B "$BUILD_DIR_FULL_PATH"
 
-PROJECT_OPTION="project"
+PROJECT_OPTION="app/"
 if [[ "${BUILD_OPTION}" == "${PROJECT_OPTION}" ]]; then
     # buildProject()
     cmake --build "${BUILD_DIR_FULL_PATH}" --target ${EXECUTABLE_NAME} # 'Build a Project'
@@ -51,16 +50,33 @@ if [[ "${BUILD_OPTION}" == "${PROJECT_OPTION}" ]]; then
     valgrind --show-error-list=yes --leak-check=full --show-leak-kinds=all "${EXECUTABLE_FULL_PATH}"
 fi
 
-TEST_OPTION="test"
+TEST_OPTION="tests/"
 if [[ "${BUILD_OPTION}" == "${TEST_OPTION}" ]]; then
     # buildUnitTests()
     cmake --build "${BUILD_DIR_FULL_PATH}" --target ${EXECUTABLE_NAME}_library # 'Build a library for the Project' - not necessary to be explicitely built - just for expressiveness
     cmake --build "${BUILD_DIR_FULL_PATH}" --target unit_tests # 'Build a library for the Project'
 
+
     # analyzeUnitTestsRuntime()
+    echo
     echo "==========================================="
     echo "        UNIT-TESTS OUTPUT + MEMCHECK"
     echo "==========================================="
+    echo
 
     valgrind --show-error-list=yes --leak-check=full --show-leak-kinds=all "./build/bin/unit_tests"
+
+    # analyzeCodeCoverageOfUnitTests() # always run after valgrind execution because if coverage runs before valgrind,
+                                       # the coverage results show 0% coverage for every source file
+    echo
+    echo
+    echo "==========================================="
+    echo "         UNIT-TESTS CODE COVERAGE"
+    echo "==========================================="
+    echo
+
+    rm *.gcov
+    gcov "${BUILD_DIR_FULL_PATH}"/app/CMakeFiles/${EXECUTABLE_NAME}_library.dir/src/* | grep "EmployeeManagementSystem" -A1
+    mkdir -p "${BUILD_DIR_FULL_PATH}"/gcov-unit_tests_code_coverage_results/
+    mv *.gcov "${BUILD_DIR_FULL_PATH}"/gcov-unit_tests_code_coverage_results/
 fi
